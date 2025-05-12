36c5395d6dee8fecbc77c772c126f972cac987e4
Esmaeil Hamdami <78254832+esmaeilhamdami@users.noreply.github.com>
2025-05-12 11:05:54 +0330

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1586470301"
+    serviceWorkerVersion: "101646024"
   }
 });
